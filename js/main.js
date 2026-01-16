f89a9a81cdf1bcfa40aa0f8f06e84a7d0a294c96
frederikkepv <frederikke.pv@gmail.com>
2026-01-16 01:53:03 +0100

    Add files via upload
    Added logo updates

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-// BURGER MENU TOGGLE
 const menuToggle = document.getElementById("menuToggle");
 const navLinks = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("show");
 });
-// Tilføjer kortvarig visuel feedback ved klik
 const buttons = document.querySelectorAll('.button, .nav-button, .cta-button');
 
 buttons.forEach(btn => {
